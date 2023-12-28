e70966d947f13e2e02cb181941ec3f980a93a80f
Konrad Olszewski <7300902+chinski99@users.noreply.github.com>
2023-12-28 17:11:53 +0100

    added image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image'
+import facepalm from '../../public/facepalm.webp'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-left justify-between p-12">
-      <p className='bg-slate-100 p-4'>Well, hello there...</p>
+      <Image src={facepalm}
+        alt='facepalm'
+      />
+      <p className='bg-slate-100 dark:bg-slate-900 p-4'>Well, hello there...</p>
 
     </main>
   )

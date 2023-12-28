4310dbcea4c147ef0861d02f79623e181aea2203
Konrad Olszewski <7300902+chinski99@users.noreply.github.com>
2023-12-28 23:16:23 +0100

    rebuilt db connection

--- src/app/api/secret/route.tsx
+++ src/app/api/secret/route.tsx
@@ -1,15 +1,16 @@
 export const dynamic = 'force-dynamic'
 
 import { Invoice } from '@/lib/definitions'
-import sql from '@/lib/db.js'
+import { get_sql } from '@/lib/db.js'
 
 async function getInvoiceData(request: Request) {
-  const res = await sql<Invoice[]>`select * from invoices`;
-  return res
+    const sql = await get_sql()
+    const res = await sql <Invoice[]>`select * from invoices`;
+    return res
 }
 
 
 export async function GET(request: Request) {
-  const invoices = await getInvoiceData(request);
-  return Response.json(invoices)
+    const invoices = await getInvoiceData(request);
+    return Response.json(invoices)
 }
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,29 +1,39 @@
-export const dynamic = "force-dynamic"
+import postgres from "postgres";
+import { get_secret } from "@/lib/secrets";
 
-import postgres from "postgres"
-import { get_secret } from "@/lib/secrets"
+const environment = process.env["NODE_ENV"];
+console.log("Environment: ", environment);
 
-const environment = process.env["NODE_ENV"]
-console.log('Environment: ', environment)
+let connection=null
 
-var config = {
-  user: process.env.PGSQL_USER,
-  password: process.env.PGSQL_PASSWORD,
-  host: process.env.PGSQL_HOST,
-  port: process.env.PGSQL_PORT,
-  database: process.env.PGSQL_DATABASE,
-  ssl: false,
+async function prepare_config() {
+  var config = {
+    user: process.env.PGSQL_USER,
+    password: process.env.PGSQL_PASSWORD,
+    host: process.env.PGSQL_HOST,
+    port: process.env.PGSQL_PORT,
+    database: process.env.PGSQL_DATABASE,
+    ssl: false,
+  };
+
+  if (environment == "production") {
+    config.ssl = { rejectUnauthorized: false };
+    const secret = process.env.PGSQL_CREDENTIALS;
+    const creds = await get_secret(secret, "eu-west-1");
+    const creds_json = JSON.parse(creds);
+    config.user = creds_json?.username ?? "";
+    config.password = creds_json?.password ?? "";
+  }
+  return config
 }
 
-if (environment == "production") {
-  config.ssl = { rejectUnauthorized: false }
-  const secret = process.env.PGSQL_CREDENTIALS
-  const creds = await get_secret(secret, "eu-west-1")
-  const creds_json = JSON.parse(creds)
-  config.user = creds_json?.username ?? ""
-  config.password = creds_json?.password ?? ""
+export async function get_sql() {
+    if (connection==null) {
+        console.debug('Creating DB connection')
+        const config = await prepare_config()
+        connection = postgres(config)
+    }
+    return connection
 }
 
-const sql = postgres(config) // will use psql environment variables
-const res = await sql`select version()`
-export default sql
+

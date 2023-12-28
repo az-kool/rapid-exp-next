93399fe7f2a4524fb980023b5732bd97f4841358
Konrad Olszewski <7300902+chinski99@users.noreply.github.com>
2023-12-28 20:40:08 +0100

    first api route

---
+++ src/app/api/envs/route.tsx
@@ -0,0 +1,10 @@
+export const dynamic = 'force-dynamic'
+
+export async function GET(request: Request) {
+    const host = process.env['PGSQL_HOST']
+    const env = process.env['NODE_ENV']
+    try {
+        return Response.json({ environment: env, host: host })
+    }
+    catch (r) { console.error(r) }
+}
